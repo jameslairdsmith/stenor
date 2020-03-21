ca6470fd66518379eedd77465b602369067b84ed
James Laird-Smith <jameslairdsmith@gmail.com>
2020-03-21 11:57:26 +0000

    clean up imports

--- R/stenor-package.R
+++ R/stenor-package.R
@@ -5,19 +5,13 @@
 # roxygen namespace tags. Modify with care!
 ## usethis namespace: start
 #' @importFrom jsonlite fromJSON
-#' @importFrom purrr flatten_dfc
-#' @importFrom purrr map
-#' @importFrom tibble as_tibble
-#' @importFrom tibble tibble
+#' @importFrom purrr map flatten_dfc
+#' @importFrom tibble tibble as_tibble rownames_to_column
 #' @importFrom magrittr %>%
 #' @importFrom fs dir_ls
 #' @importFrom tidyr unnest
-#' @importFrom dplyr mutate
-#' @importFrom dplyr select
-#' @importFrom stringr str_remove
-#' @importFrom stringr str_extract
-#' @importFrom tibble rownames_to_column
-#' @importFrom dplyr rename
+#' @importFrom dplyr mutate select rename
+#' @importFrom stringr str_remove str_extract
 #' @importFrom readr read_tsv
 ## usethis namespace: end
 NULL
