c4954ad3089d48ebaffd54f5f4e0df89fe13e8c6
James Laird-Smith <jameslairdsmith@gmail.com>
2020-05-20 19:21:34 +0100

    added function for creating a Plover dictionary

--- R/dictionary.R
+++ R/dictionary.R
@@ -28,3 +28,36 @@ read_plover_dir <- function(dir){
     select(-dir)
 
 }
+
+#' @export
+
+write_plover_dict <- function(df, output_dir = NULL){
+
+  ## must have two columns, 'stroke' and 'translation'
+
+  out <-
+    df %>%
+    select(stroke, translation) %>%
+    mutate_at(vars(stroke, translation), as.character) %>%
+    mutate(rowlist = map2(stroke, translation, make_named_list)) %>%
+    pull(rowlist) %>%
+    flatten() %>%
+    toJSON(pretty = T,
+           auto_unbox = F) %>%
+    str_replace_all("\\[", "") %>%
+    str_replace_all("\\]", "")
+
+  if(!is.null(output_dir)){
+    write(out, output_dir)
+    message("Output written to ", output_dir)
+  }
+
+  out
+}
+
+make_named_list <- function(col_for_names, col_for_values){
+
+  out <- list(col_for_values)
+  names(out) <- col_for_names
+  out
+}
--- R/stenor-package.R
+++ R/stenor-package.R
@@ -4,14 +4,14 @@
 # The following block is used by usethis to automatically manage
 # roxygen namespace tags. Modify with care!
 ## usethis namespace: start
-#' @importFrom jsonlite fromJSON
-#' @importFrom purrr map flatten_dfc
+#' @importFrom jsonlite fromJSON toJSON
+#' @importFrom purrr map flatten_dfc map2 flatten
 #' @importFrom tibble tibble as_tibble rownames_to_column
 #' @importFrom magrittr %>%
 #' @importFrom fs dir_ls
 #' @importFrom tidyr unnest
-#' @importFrom dplyr mutate select rename
-#' @importFrom stringr str_remove str_extract
+#' @importFrom dplyr mutate select rename mutate_at vars pull
+#' @importFrom stringr str_remove str_extract str_replace_all
 #' @importFrom readr read_tsv
 ## usethis namespace: end
 NULL
