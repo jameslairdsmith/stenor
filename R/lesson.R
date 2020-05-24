0a4c49dab32333f37ce0cf65ef2725c8a1f29e2c
James Laird-Smith <jameslairdsmith@gmail.com>
2020-05-24 08:40:59 +0100

    cleaned up lesson functions

--- R/lesson.R
+++ R/lesson.R
@@ -1,22 +1,15 @@
 #' @export
+#' @importFrom readr read_tsv
 
-read_lesson <- function(tsv_file,
-                        col_names = c("result", "strokes"),
-                        ...){
+read_lesson <- function(tsv_file, col_names = c("result", "strokes"), ...){
 
-  read_tsv(tsv_file,
-           col_names = col_names,
-           col_types = "cc",
-           ...)
+  read_tsv(tsv_file, col_names = col_names, col_types = "cc", ...)
 }
 
 #' @export
+#' @importFrom readr write_tsv
 
 write_lesson <- function(x, output_dir, ...){
 
-  write_tsv(x,
-            output_dir,
-            append = F,
-            col_names = F,
-            ...)
+  write_tsv(x, output_dir, append = FALSE, col_names = FALSE, ...)
 }
