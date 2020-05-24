413d84105a09d4495e5e2a0e813e1fe7982dfcf9
James Laird-Smith <jameslairdsmith@gmail.com>
2020-05-24 09:31:48 +0100

    expanded on functions for working with Google sheets

--- R/sheets.R
+++ R/sheets.R
@@ -1,6 +1,29 @@
+#' @importFrom purrr map_dfr
+#' @export
+
+read_sheet_dictionary <- function(spreadsheet_id){
+
+  sheet_names <- get_sheet_names(spreadsheet_id)
+
+  map_dfr(sheet_names, read_steno_sheet, spreadsheet_id = spreadsheet_id)
+
+}
+
+
+#' @importFrom dplyr everything
+#' @importFrom googlesheets4 read_sheet
+#' @export
+
+read_steno_sheet <- function(spreadsheet_id, sheet_name){
+
+  read_sheet(spreadsheet_id, sheet = sheet_name, col_types = "c") %>%
+    mutate(dictionary_name = sheet_name) %>%
+    select(dictionary_name, everything())
+}
+
 #' @importFrom googlesheets4 gs4_get
 #' @importFrom magrittr extract2
-#' @importFrom dplyr %>% pull
+#' @importFrom dplyr pull
 #' @export
 
 get_sheet_names <- function(sheet_id){
