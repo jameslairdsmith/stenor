0f7aa98d0507b57ea2d4c75ed3199a6d9fe13607
James Laird-Smith <jameslairdsmith@gmail.com>
2021-01-24 10:53:59 +0000

    fix `slice_lesson()`

--- R/lesson.R
+++ R/lesson.R
@@ -14,8 +14,8 @@ write_lesson <- function(x, output_dir, ...){
   write_tsv(x, output_dir, append = FALSE, col_names = FALSE, ...)
 }
 
-#'  @importFrom dplyr slice
-#'  @export
+#' @export
+#' @importFrom dplyr slice
 
 slice_lesson <- function(data, end_index, window_size = 5L){
 
@@ -25,3 +25,5 @@ slice_lesson <- function(data, end_index, window_size = 5L){
 
   slice(data, seq(start_index, end_index, by = 1L))
 }
+
+
