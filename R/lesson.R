0a10f87be2eceb4059f73bc2930419e2d061ecd7
James Laird-Smith <jameslairdsmith@gmail.com>
2021-01-24 11:28:25 +0000

    tweaks to lesson functions

--- R/lesson.R
+++ R/lesson.R
@@ -9,9 +9,13 @@ read_lesson <- function(tsv_file, col_names = c("result", "strokes"), ...){
 #' @export
 #' @importFrom readr write_tsv
 
-write_lesson <- function(x, output_dir, ...){
+write_lesson <- function(data, output_dir, ...){
 
-  write_tsv(x, output_dir, append = FALSE, col_names = FALSE, ...)
+  write_tsv(data, output_dir, append = FALSE, col_names = FALSE, ...)
+
+  message(paste0("Data sucsessfully written to `output_dir`."))
+
+  data
 }
 
 #' @export
@@ -29,9 +33,13 @@ slice_lesson <- function(data, end_index, window_size = 5L){
 #' @export
 #' @importFrom clipr write_clip
 
-clip_lesson <- function(data){
+clip_lesson <- function(data, col_names = FALSE, return_new = TRUE ,...){
 
-  write_clip(data, col.names = FALSE)
+  write_clip(data, col.names = col_names, return_new = return_new, ...)
+
+  message(paste("Data sucsessfully written to clipboard."))
+
+  data
 }
 
 
