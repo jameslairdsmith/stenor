be0dcee2f693b97485139dc192cec344bac10f64
James Laird-Smith <jameslairdsmith@gmail.com>
2021-01-24 13:55:04 +0000

    clip_practice() now uses start_index

--- R/practice.R
+++ R/practice.R
@@ -1,12 +1,13 @@
 #' @export
 
-clip_practice <- function(end_index,
+clip_practice <- function(start_index,
                           window_size = 5L,
                           dir = here("temp", "practice.tsv")){
 
   dir %>%
     read_lesson() %>%
-    slice_lesson(end_index = end_index, window_size = window_size) %>%
+    slice_lesson(start_index = start_index,
+                 window_size = window_size) %>%
     clip_lesson()
 }
 
